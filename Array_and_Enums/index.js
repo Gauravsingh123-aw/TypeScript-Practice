70b766d66d587b806113c937c0779bebadb45b00
Singh <2308859@cognizant.com>
2025-09-02 21:16:33 +0530

    added array and enum folder

---
+++ Array_and_Enums/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let arr = [1, 2, 4];
+let arr2 = [3, 4, 55,];
+//when array has different elements
+let c = ["a", 11, 221354];
+//# sourceMappingURL=index.js.map
---
+++ Array_and_Enums/index.ts
@@ -0,0 +1,6 @@
+let arr:number[]=[1,2,4]
+let arr2:Array<number>=[3,4,55,];
+
+//when array has different elements
+
+let c:(string|number)[]=["a",11,221354];
